3db1dbd6d94133f6720b3b6b5770cd7a51be1d8a
ghost <phantom17pc@gmail.com>
2014-10-24 12:51:10 -0400

    first commit

---
+++ Avengers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+//comments
+public class Avengers{
+	public static void main (String [] args){
+		System.out.println("The Avengers Initiative...");
+		Scanner a = new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int num = a.nextInt();
+		System.out.println(num*num*num);
+	}
+}
